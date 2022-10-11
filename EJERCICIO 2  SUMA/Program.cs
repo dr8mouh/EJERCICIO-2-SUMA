ac2fd8ba6165999566129a29ea2d979052eb19a1
LENOVO <LENOVO@DESKTOP-02MC7HS>
2022-10-11 00:44:56 -0500

    Agregar archivos de proyecto.

---
+++ EJERCICIO 2  SUMA/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+namespace Ejercicio_2
+{
+    class programa_2
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int resultado;
+
+            System.Console.WriteLine("Ingresa el número 1: ");
+            num1 = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Ingresa el número 2: ");
+            num2 = int.Parse(Console.ReadLine());
+            resultado = num1 + num2;
+
+            System.Console.WriteLine("El resultado es: " + resultado);
+            Console.ReadLine();
+        }
+    }
+}
